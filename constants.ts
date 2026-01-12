e72259f82e3782acd5aa8ed59a585b87ca6d3ded
Skeptic69 <109815957+Skeptic69@users.noreply.github.com>
2026-01-12 16:18:11 +0530

    Update X community link to specific status URL

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 export const COIN_ADDRESS = "0xff13322caaff267988cdb980fddf7fc0c8d04444";
 export const DEXSCREENER_URL = "https://dexscreener.com/bsc/0x0ece01b7b1ad051cee9ab980bcdda78be70c9cb0";
 export const DEXSCREENER_EMBED_URL = "https://dexscreener.com/bsc/0x0ece01b7b1ad051cee9ab980bcdda78be70c9cb0?embed=1&theme=dark";
-export const TWITTER_URL = "https://twitter.com";
+export const TWITTER_URL = "https://x.com/i/status/2010619730164678679";
 export const TELEGRAM_URL = "https://telegram.org";
 export const BUY_URL = "https://dexscreener.com/bsc/0x0ece01b7b1ad051cee9ab980bcdda78be70c9cb0" + COIN_ADDRESS;
 
